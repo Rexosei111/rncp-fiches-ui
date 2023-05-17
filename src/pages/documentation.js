aa1f277ef35cb7098fcd9881d3c642eec63526de
Kyei Samuel <kyeisamuel931@gmail.com>
2023-05-17 13:43:14 +0000

    fix docs

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "../components/layout";
 import Head from "next/head";
-import { RedocStandalone } from "redoc";
+import { Box } from "@mui/material";
+import Iframe from "react-iframe";
+// import { RedocStandalone } from "redoc";
 
 export default function APIDocs() {
   return (
@@ -9,12 +11,24 @@ export default function APIDocs() {
       <Head>
         <title>API Documentation</title>
       </Head>
-      <RedocStandalone
+      <Box width={"100%"} height={"89.8vh"}>
+        <Iframe
+          frameBorder={0}
+          allowFullScreen
+          overflow="hidden"
+          display="block"
+          title="API Documentation"
+          width="100%"
+          height="100%"
+          url={`${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/redoc`}
+        ></Iframe>
+      </Box>
+      {/* <RedocStandalone
         specUrl={`${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/fiches-openapi`}
         options={{
           theme: { colors: { primary: { main: "#FF8300" } } },
         }}
-      />
+      /> */}
     </>
   );
 }
