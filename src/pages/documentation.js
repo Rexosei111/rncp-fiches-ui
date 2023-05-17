1b01691f2f6efb690cbd156a3807363c66815c64
Kyei Samuel <kyeisamuel931@gmail.com>
2023-05-17 14:29:37 +0000

    switch to redoc component

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import Head from "next/head";
-import { Box } from "@mui/material";
-import Iframe from "react-iframe";
-// import { RedocStandalone } from "redoc";
+import { RedocStandalone } from "redoc";
 
 export default function APIDocs() {
   return (
@@ -11,24 +9,12 @@ export default function APIDocs() {
       <Head>
         <title>API Documentation</title>
       </Head>
-      <Box width={"100%"} height={"89.8vh"}>
-        <Iframe
-          frameBorder={0}
-          allowFullScreen
-          overflow="hidden"
-          display="block"
-          title="API Documentation"
-          width="100%"
-          height="100%"
-          url={`${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/fiches-redoc`}
-        ></Iframe>
-      </Box>
-      {/* <RedocStandalone
+      <RedocStandalone
         specUrl={`${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/fiches-openapi`}
         options={{
           theme: { colors: { primary: { main: "#FF8300" } } },
         }}
-      /> */}
+      />
     </>
   );
 }
