ed2cad97e33fa884c42750c34e130380ad8688fb
Kyei Samuel <kyeisamuel931@gmail.com>
2023-05-18 23:16:44 +0000

    initial props

--- src/pages/fiches/[rncp].js
+++ src/pages/fiches/[rncp].js
@@ -7,8 +7,9 @@ import {
   Stack,
   Tooltip,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
@@ -25,6 +26,8 @@ import Partenaires from "@/components/components/details/partenaires";
 import CertDescription from "@/components/components/details/certDescription";
 import BlocCompetence from "@/components/components/details/blocCompetence";
 import StatistiqueTable from "@/components/components/details/statistiqueTable";
+import Image from "next/image";
+import fiche404 from "../../../public/404.svg";
 
 const LatoStyle = Lato({
   subsets: ["latin"],
@@ -36,13 +39,17 @@ export default function FicheDetails() {
   const [loading, setLoading] = useState(false);
   const [rncpData, setRncpData] = useState({});
   const [blocDate, setBlocDate] = useState({});
+  const [notFound, setNotFound] = useState(false);
+  const small = useMediaQuery((theme) => theme.breakpoints.down("md"));
+  const xsmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
   const { rncp } = router.query;
   useEffect(() => {
     async function fetchRncp() {
       const { rncp: numero_fiche } = router.query;
       setLoading(true);
       try {
-        const { data } = await axios.get(
+        const { data, status } = await axios.get(
           process.env.NEXT_PUBLIC_API_BASE_URL +
             "/private/fiches/" +
             numero_fiche
@@ -50,6 +57,11 @@ export default function FicheDetails() {
         setLoading(false);
         setRncpData(data);
       } catch (error) {
+        if (isAxiosError(error)) {
+          if (error.response.status === 404) {
+            setNotFound(true);
+          }
+        }
         console.log(error);
         setLoading(false);
       }
@@ -81,180 +93,210 @@ export default function FicheDetails() {
       date_limite_delivrance: rncpData?.date_limite_delivrance,
     });
   }, [rncpData]);
+  if (loading) {
+    return <DetailSkeleton />;
+  }
+
+  if (notFound) {
+    return (
+      <>
+        <Head>
+          <title>{rncp}</title>
+        </Head>
+        <Container
+          maxWidth={"lg"}
+          sx={{
+            minHeight: "79vh",
+            py: { xs: 0, md: 2 },
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="h6"
+            textTransform={"capitalize"}
+            fontSize={40}
+            fontWeight={700}
+            textAlign={"center"}
+          >
+            Fiche Not Found
+          </Typography>
+          <Image
+            src={fiche404}
+            height={xsmall ? 200 : small ? 300 : 350}
+            alt="404"
+          />
+        </Container>
+      </>
+    );
+  }
   return (
     <>
       <Head>
         <title>{rncp}</title>
       </Head>
-      {loading && <DetailSkeleton />}
-      {!loading && (
-        <Container maxWidth={"lg"} sx={{ minHeight: "79vh", py: 2 }}>
+
+      <Container maxWidth={"lg"} sx={{ minHeight: "79vh", py: 2 }}>
+        <Stack alignItems={"center"} justifyContent={"center"} minHeight={350}>
           <Stack
-            alignItems={"center"}
-            justifyContent={"center"}
-            minHeight={350}
+            component={Paper}
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            gap={{ xs: 1, md: 5 }}
+            p={{ xs: 1, md: 3 }}
+            elevation={10}
+            width={"100%"}
+            flexWrap={{ xs: "wrap", md: "nowrap" }}
+            sx={{ minHeight: 300, borderRadius: 3 }}
           >
             <Stack
-              component={Paper}
-              flexDirection={"row"}
-              justifyContent={"space-between"}
-              gap={{ xs: 1, md: 5 }}
-              p={{ xs: 1, md: 3 }}
-              elevation={10}
               width={"100%"}
-              flexWrap={{ xs: "wrap", md: "nowrap" }}
-              sx={{ minHeight: 300, borderRadius: 3 }}
+              flexDirection={"column"}
+              justifyContent={"center"}
             >
+              <Tooltip title={rncpData?.intitule}>
+                <Typography
+                  variant="h1"
+                  fontSize={
+                    rncpData?.intitule && calculateFontSize(rncpData?.intitule)
+                  }
+                  fontWeight={600}
+                  textAlign={"center"}
+                  fontFamily={LatoStyle.style.fontFamily}
+                  gutterBottom
+                >
+                  {rncpData?.intitule}
+                </Typography>
+              </Tooltip>
               <Stack
-                width={"100%"}
-                flexDirection={"column"}
+                width="100%"
+                flexDirection={"row"}
                 justifyContent={"center"}
+                alignItems={"center"}
               >
-                <Tooltip title={rncpData?.intitule}>
-                  <Typography
-                    variant="h1"
-                    fontSize={
-                      rncpData?.intitule &&
-                      calculateFontSize(rncpData?.intitule)
-                    }
-                    fontWeight={600}
-                    textAlign={"center"}
-                    fontFamily={LatoStyle.style.fontFamily}
-                    gutterBottom
-                  >
-                    {rncpData?.intitule}
+                <Breadcrumbs separator="-">
+                  <Typography textAlign={"center"} color={"#5A606F"}>
+                    {rncpData?.numero_fiche}
                   </Typography>
-                </Tooltip>
-                <Stack
-                  width="100%"
-                  flexDirection={"row"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                >
-                  <Breadcrumbs separator="-">
-                    <Typography textAlign={"center"} color={"#5A606F"}>
-                      {rncpData?.numero_fiche}
-                    </Typography>
-                    <Stack flexDirection={"row"} alignItems={"center"} gap={1}>
-                      <Typography
-                        color={rncpData?.statut ? "#00C408" : "#F0000E"}
-                      >
-                        {rncpData.statut ? "Fiche Active" : "Fiche Inactive"}{" "}
-                      </Typography>
-                      {rncpData.statut ? (
-                        <CheckCircleOutlineRoundedIcon
-                          fontSize="small"
-                          htmlColor="#00C408"
-                        />
-                      ) : (
-                        <HighlightOffRoundedIcon
-                          fontSize="small"
-                          htmlColor="#F0000E"
-                        />
-                      )}
-                    </Stack>
-                  </Breadcrumbs>
-                </Stack>
-                <Stack
-                  width={"100%"}
-                  flexDirection={{ xs: "column", md: "row" }}
-                  // flexWrap={{ xs: "wrap", lg: "nowrap" }}
-                  justifyContent={{ xs: "center", md: "space-between" }}
-                  my={2}
-                >
-                  {rncpData?.ancienne_certification && (
+                  <Stack flexDirection={"row"} alignItems={"center"} gap={1}>
                     <Typography
-                      variant="caption"
-                      fontSize={14}
-                      color={"#5A606F"}
-                      component={Link}
-                      textAlign={"center"}
-                      href={"/fiches/" + rncpData?.ancienne_certification}
+                      color={rncpData?.statut ? "#00C408" : "#F0000E"}
                     >
-                      Certification antérieure :{" "}
-                      {rncpData?.ancienne_certification}
+                      {rncpData.statut ? "Fiche Active" : "Fiche Inactive"}{" "}
                     </Typography>
-                  )}
-                  {rncpData?.nouvelle_certification && (
-                    <Typography
-                      variant="caption"
-                      fontSize={14}
-                      color={"#5A606F"}
-                      component={Link}
-                      textAlign={"center"}
-                      ml={{ xs: 0, md: "auto" }}
-                      href={"/fiches/" + rncpData?.nouvelle_certification}
-                    >
-                      Remplacée par : {rncpData?.nouvelle_certification}
-                    </Typography>
-                  )}
-                </Stack>
+                    {rncpData.statut ? (
+                      <CheckCircleOutlineRoundedIcon
+                        fontSize="small"
+                        htmlColor="#00C408"
+                      />
+                    ) : (
+                      <HighlightOffRoundedIcon
+                        fontSize="small"
+                        htmlColor="#F0000E"
+                      />
+                    )}
+                  </Stack>
+                </Breadcrumbs>
               </Stack>
               <Stack
                 width={"100%"}
-                p={2}
-                alignItems={{ xs: "flex-start", md: "center" }}
-                justifyContent={"center"}
-                flexDirection={"column"}
+                flexDirection={{ xs: "column", md: "row" }}
+                // flexWrap={{ xs: "wrap", lg: "nowrap" }}
+                justifyContent={{ xs: "center", md: "space-between" }}
+                my={2}
               >
-                <BlocDate data={blocDate} />
+                {rncpData?.ancienne_certification && (
+                  <Typography
+                    variant="caption"
+                    fontSize={14}
+                    color={"#5A606F"}
+                    component={Link}
+                    textAlign={"center"}
+                    href={"/fiches/" + rncpData?.ancienne_certification}
+                  >
+                    Certification antérieure :{" "}
+                    {rncpData?.ancienne_certification}
+                  </Typography>
+                )}
+                {rncpData?.nouvelle_certification && (
+                  <Typography
+                    variant="caption"
+                    fontSize={14}
+                    color={"#5A606F"}
+                    component={Link}
+                    textAlign={"center"}
+                    ml={{ xs: 0, md: "auto" }}
+                    href={"/fiches/" + rncpData?.nouvelle_certification}
+                  >
+                    Remplacée par : {rncpData?.nouvelle_certification}
+                  </Typography>
+                )}
               </Stack>
+            </Stack>
+            <Stack
+              width={"100%"}
+              p={2}
+              alignItems={{ xs: "flex-start", md: "center" }}
+              justifyContent={"center"}
+              flexDirection={"column"}
+            >
+              <BlocDate data={blocDate} />
             </Stack>
           </Stack>
-          <Stack flexDirection={"column"} gap={5} my={3}>
-            <Stack
-              gap={5}
-              flexDirection={"row"}
-              flexWrap={{ xs: "wrap", md: "nowrap" }}
-            >
-              <CardWrapper title={"Index"} id={"indexes"}>
-                <Indexes />
-              </CardWrapper>
-              <CardWrapper title={"Informations de référence"} id={"reference"}>
-                <Infos fiche={rncpData} />
-              </CardWrapper>
-            </Stack>
-            <Stack
-              gap={5}
-              flexDirection={"row"}
-              my={2}
-              flexWrap={{ xs: "wrap", md: "nowrap" }}
-            >
-              <CardWrapper title={"Certificateur"} id={"certificateur"}>
-                <Certificateurs
-                  certificateurs={rncpData?.fiches_certificateurs}
-                />
-              </CardWrapper>
-              <CardWrapper title={"Partenaire"} id={"partenaires"}>
-                <Partenaires partenaires={rncpData?.fiches_partenaires} />
-              </CardWrapper>
-            </Stack>
-            <CardWrapper
-              title={"Description de la certification"}
-              width={"100%"}
-              id={"certDescription"}
-            >
-              <CertDescription descriptions={rncpData?.fiches_descriptions} />
+        </Stack>
+        <Stack flexDirection={"column"} gap={5} my={3}>
+          <Stack
+            gap={5}
+            flexDirection={"row"}
+            flexWrap={{ xs: "wrap", md: "nowrap" }}
+          >
+            <CardWrapper title={"Index"} id={"indexes"}>
+              <Indexes />
             </CardWrapper>
-            <CardWrapper
-              title={"Blocs de compétences"}
-              width={"100%"}
-              id={"blocCompetence"}
-            >
-              <BlocCompetence code_blocs={rncpData?.blocs_competences} />
+            <CardWrapper title={"Informations de référence"} id={"reference"}>
+              <Infos fiche={rncpData} />
             </CardWrapper>
-            <CardWrapper
-              title={"Statistiques"}
-              width={"100%"}
-              id={"statistiques"}
-            >
-              <StatistiqueTable
-                statistiques={rncpData?.statistiques_promotion}
+          </Stack>
+          <Stack
+            gap={5}
+            flexDirection={"row"}
+            my={2}
+            flexWrap={{ xs: "wrap", md: "nowrap" }}
+          >
+            <CardWrapper title={"Certificateur"} id={"certificateur"}>
+              <Certificateurs
+                certificateurs={rncpData?.fiches_certificateurs}
               />
             </CardWrapper>
+            <CardWrapper title={"Partenaire"} id={"partenaires"}>
+              <Partenaires partenaires={rncpData?.fiches_partenaires} />
+            </CardWrapper>
           </Stack>
-        </Container>
-      )}
+          <CardWrapper
+            title={"Description de la certification"}
+            width={"100%"}
+            id={"certDescription"}
+          >
+            <CertDescription descriptions={rncpData?.fiches_descriptions} />
+          </CardWrapper>
+          <CardWrapper
+            title={"Blocs de compétences"}
+            width={"100%"}
+            id={"blocCompetence"}
+          >
+            <BlocCompetence code_blocs={rncpData?.blocs_competences} />
+          </CardWrapper>
+          <CardWrapper
+            title={"Statistiques"}
+            width={"100%"}
+            id={"statistiques"}
+          >
+            <StatistiqueTable statistiques={rncpData?.statistiques_promotion} />
+          </CardWrapper>
+        </Stack>
+      </Container>
     </>
   );
 }
