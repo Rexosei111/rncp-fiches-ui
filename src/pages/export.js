ce87e58c764d26e7c7382efa973efb8249d1e90e
Kyei Samuel <kyeisamuel931@gmail.com>
2023-05-23 10:11:18 +0000

    export page updated

--- src/pages/export.js
+++ src/pages/export.js
@@ -1,39 +1,61 @@
-import React from 'react'
-import Layout from "../components/layout"
-import { Box, Button, Container, Stack, Typography } from '@mui/material'
-import Head from 'next/head';
-import exportImg from "../../public/export.svg"
-import Image from 'next/image';
+import React from "react";
+import Layout from "../components/layout";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Head from "next/head";
+import exportImg from "../../public/export.svg";
+import Image from "next/image";
 export default function Export() {
   return (
     <>
-    <Head>
+      <Head>
         <title>Export</title>
-    </Head>
-    <Container maxWidth={"lg"} sx={{ minHeight: "79vh", py: 2 }}>
-        <Stack flexDirection={{xs: "column", md: "row"}}  gap={2} minHeight={"50vh"}>
-            <Box width={{xs: "100%", md: "50%"}}>
-                <Typography variant='h3' gutterBottom color={(theme) => theme.palette.primary.main}>
-                Besoin d'exports personnalisés de la data présente sur le site ?
-                </Typography>
-                <Typography variant='subtitle2' fontSize={15}>
-                Besoin d'associer à la data du site des informations complémentaires ?
-                </Typography>
-                <Typography variant='subtitle2' fontSize={15}>
-                Notre équipe d'analyste peut vous accompagner.
-                </Typography>
-                <Button variant="outlined" sx={{ mt: 2}} component={"a"} target='_blank' href={`mailto:alexis.dana@formadata.fr`}>Contactez-nous</Button>
-            </Box>
-            <Box width={{xs: "100%", md: "50%"}} height={"50vh"} position="relative">
-                <Image src={exportImg} fill style={{objectFit: "contain"}} /> 
-            </Box>
+      </Head>
+      <Container maxWidth={"lg"} sx={{ minHeight: "79vh", py: 2 }}>
+        <Stack
+          flexDirection={{ xs: "column", md: "row" }}
+          gap={2}
+          minHeight={"inherit"}
+          justifyContent={{ xs: "center", md: "center" }}
+          alignItems={{ xs: "center", md: "center" }}
+        >
+          <Box width={{ xs: "100%", md: "50%" }}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color={(theme) => theme.palette.primary.main}
+            >
+              Besoin d'exports personnalisés de la data présente sur le site ?
+            </Typography>
+            <Typography variant="subtitle2" fontSize={15}>
+              Besoin d'associer à la data du site des informations
+              complémentaires ?
+            </Typography>
+            <Typography variant="subtitle2" fontSize={15}>
+              Notre équipe d'analyste peut vous accompagner.
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{ mt: 2 }}
+              component={"a"}
+              target="_blank"
+              href={`mailto:alexis.dana@formadata.fr`}
+            >
+              Contactez-nous
+            </Button>
+          </Box>
+          <Box
+            width={{ xs: "0%", sm: "100%", md: "50%" }}
+            height={{ xs: 0, sm: "50vh" }}
+            position="relative"
+          >
+            <Image src={exportImg} fill style={{ objectFit: "contain" }} />
+          </Box>
         </Stack>
-    </Container>
+      </Container>
     </>
-  )
+  );
 }
 
-
 Export.getLayout = function (page) {
-    return <Layout>{page}</Layout>;
-  };
+  return <Layout>{page}</Layout>;
+};
